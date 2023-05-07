c531c59b3a2eb0be5f736bc0ae8d0f206aac9b40
George Osborn <georgeosborn2002@gmail.com>
2023-05-07 11:15:25 +0100

    Add logging in place of previous logging 'solution' that didn't work properly for having multiple loggers

--- src/com/Schoolio/Launcher.java
+++ src/com/Schoolio/Launcher.java
@@ -1,11 +1,25 @@
 package com.Schoolio;
 
+import com.Schoolio.helpers.ConfigFile;
 import com.Schoolio.views.MainWindow;
-
-import javax.swing.*;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
 
 public class Launcher {
+
+    private static final String DEBUG_MODE = new ConfigFile().config().getProperty("DEBUG_MODE");
+
+    private static final Logger LOGGER = LogManager.getLogger("FileLogger");
+
     public static void main(String[] args) {
+        if (DEBUG_MODE.equals("on")) {
+            Configurator.setLevel("FileLogger", Level.TRACE);
+        } else {
+            Configurator.setLevel("FileLogger", Level.OFF);
+        }
+
         new MainWindow().show();
     }
 }
