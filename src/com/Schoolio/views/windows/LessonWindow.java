03814d059679811605b29c0731515116fdb0e15e
George Osborn <georgeosborn2002@gmail.com>
2023-06-02 19:11:29 +0100

    Add rounded borders to the add lesson form in the window

--- src/com/Schoolio/views/windows/LessonWindow.java
+++ src/com/Schoolio/views/windows/LessonWindow.java
@@ -5,6 +5,7 @@ import com.Schoolio.views.partials.LessonWindowInner;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 
 public class LessonWindow extends JFrame {
@@ -17,12 +18,17 @@ public class LessonWindow extends JFrame {
         setVisible(true);
 
         JPanel lessonWindowContainer = new JPanel();
-        lessonWindowContainer.setBorder(new EmptyBorder(35, 100, 35, 100));
+        lessonWindowContainer.setBorder(new EmptyBorder(35, 0, 35, 0));
         lessonWindowContainer.setLayout(new GridBagLayout());
+
+        JPanel lessonWindowBorder = new JPanel();
+        lessonWindowBorder.setBorder(new LineBorder(Color.WHITE, 36, true));
+        lessonWindowBorder.setLayout(new BoxLayout(lessonWindowBorder, BoxLayout.Y_AXIS));
 
         JPanel lessonWindowInner = new LessonWindowInner();
 
-        lessonWindowContainer.add(lessonWindowInner);
+        lessonWindowBorder.add(lessonWindowInner);
+        lessonWindowContainer.add(lessonWindowBorder);
 
         add(index.getHeaderLabel(), BorderLayout.PAGE_START);
         add(lessonWindowContainer, BorderLayout.CENTER);
