a61fd04ec58716b5a1e6a8b70e8849856122753d
George Osborn <georgeosborn2002@gmail.com>
2023-06-25 15:27:00 +0100

    Change year main column name

--- src/com/Schoolio/objects/YearObject.java
+++ src/com/Schoolio/objects/YearObject.java
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName;
 
 public class YearObject {
     @SerializedName("id") private final int id;
-    @SerializedName("year") private final String name;
+    @SerializedName("name") private final String name;
 
     public YearObject(int id, String name) {
         this.id = id;
